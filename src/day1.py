444ecdb02eed9ac718a476d787ef1d7f98e3edb4
Jaime <jamartbr@gmail.com>
2023-12-01 14:00:00 +0100

    dia 1

--- src/day1.py
+++ src/day1.py
@@ -2,7 +2,7 @@ file_path = "../data/day1.txt"
 with open(file_path, "r") as f:
 
     # Parte 1
-    
+
     s = 0
 
     lines = f.readlines()
@@ -13,21 +13,21 @@ with open(file_path, "r") as f:
 
     # Parte 2
 
-    s=0
-    n=["zero","one","two","three","four","five","six","seven","eight","nine","0","1","2","3","4","5","6","7","8","9"]
+    s = 0
+    n = ["zero","one","two","three","four","five","six","seven","eight","nine","0","1","2","3","4","5","6","7","8","9"]
 
     for line in lines:
-        a=0
-        b=0
-        _min=len(line)
-        _max=-1
+        a = 0
+        b = 0
+        _min = len(line)
+        _max = -1
         for i in n:
             if line.find(i)!=-1 and _min>line.find(i):
-                _min=line.find(i)
-                a=n.index(i)%10
+                _min = line.find(i)
+                a = n.index(i)%10
                 
             if line.rfind(i)!=-1 and _max<line.rfind(i):
-                _max=line.rfind(i)
-                b=n.index(i)%10
-        s+=int(a*10+b)
+                _max = line.rfind(i)
+                b = n.index(i)%10
+        s += int(a*10+b)
     print(s)
