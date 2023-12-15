190d18e598e6e67d0f0f5adff53bed20e0e3dd05
Jaime <jamartbr@gmail.com>
2023-12-15 19:53:07 +0100

    cambios día 11

--- src/day11.py
+++ src/day11.py
@@ -2,30 +2,21 @@ file_path = "../data/day11.txt"
 with open(file_path, "r") as f:
     lines = [x.strip() for x in f.readlines()]
 
-exp = []
-for i in range(len(lines)):
-    if "#" not in lines[i]:
-        exp.append(i)
+filasVacias = [i for i, fila in enumerate(lines) if '#' not in fila]
+colVacias = [i for i, col in enumerate(list(map(list, zip(*lines)))) if '#' not in col]
 
-colExp = []
-cols = ["".join(x) for x in list(map(list, zip(*lines)))]
-for i in range(len(cols)):
-    if "#" not in cols[i]:
-        colExp.append(i)
-
-lines = ["".join(x) for x in list(map(list, zip(*cols)))]
-
-galaxias = []
-for i in range(len(lines)):
-    for j in range(len(lines[i])):
-        if lines[i][j] == "#":
-            galaxias.append((i,j))
+galaxias = [(i,j) for i in range(len(lines)) for j in range(len(lines[i])) if lines[i][j] == "#"]
 
 p1,p2 = 0,0
-for i in range(len(galaxias)):
-    x_1,y_1 = galaxias[i]
-    p1 += sum([abs(x_2-x_1) + abs(y_2-y_1) + len(set(colExp) & set(range(min(y_1,y_2)+1,max(y_1,y_2)))) + len(set(exp) & set(range(min(x_1,x_2)+1,max(x_1,x_2)))) for x_2,y_2 in galaxias[i+1:]])
-    p2 += sum([abs(x_2-x_1) + abs(y_2-y_1) + 1000000*(len(set(colExp) & set(range(min(y_1,y_2)+1,max(y_1,y_2)))) + len(set(exp) & set(range(min(x_1,x_2)+1,max(x_1,x_2))))) for x_2,y_2 in galaxias[i+1:]])
+for k in range(len(galaxias)):
+    x_1,y_1 = galaxias[k]
+    for x_2,y_2 in galaxias[k+1:]:
+        for i in range(min(x_1,x_2),max(x_1,x_2)):
+            p1 += 2 if i in filasVacias else 1
+            p2 += 1000000 if i in filasVacias else 1
+        for j in range(min(y_1,y_2),max(y_1,y_2)):
+            p1 += 2 if j in colVacias else 1
+            p2 += 1000000 if j in colVacias else 1
 print(p1)
 print(p2)
         
